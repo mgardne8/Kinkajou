88d94321b0b357565129caf098a66698bb43bee4
MGardne8 <Matt@m-gardner.info>
2022-10-17 22:42:49 -0400

    Roughout of cage and uncage functions - added assignment data

--- kinkajou/errors.py
+++ kinkajou/errors.py
@@ -12,3 +12,11 @@ class CagingError(KinkajouError):
     def __init__(self) -> None:
         self.message = "Supplied Object could not be caged"
         super().__init__(self.message)
+
+
+class ReleasingError(KinkajouError):
+    """This exception is raised when a caged objects refuses to return to the wild"""
+
+    def __init__(self) -> None:
+        self.message = "Supplied Object could not be released"
+        super().__init__(self.message)
--- kinkajou/functions.py
+++ kinkajou/functions.py
@@ -5,8 +5,51 @@ Todo:
     Write Todo
 """
 
+import os.path
+
+import csv
+
+from kinkajou.errors import CagingError
 from kinkajou.objects import DataCage
 
 
-def cage() -> DataCage:
+def cage_csv(
+    infile: str, has_header: bool = True, encoding: str = None, **fmtparams
+) -> DataCage:
+    """Cage a CSV file from a path
+
+    Args:
+        infile (str): relative path to csv file to cage
+        has_header (bool, optional): Generate header if one doesn't exist. Defaults to True.
+        **fmtparams: any Dialect and Formatting paramaters from the CSV module -
+                    `https://docs.python.org/3/library/csv.html#csv-fmt-params` for more information
+
+    Raises:
+        CagingError: Was unable to Cage the data - it continues to run free.
+
+    Returns:
+        DataCage: Cage containing data from the provided CSV file
+    """
+
+    try:
+        infile = os.path.abspath(os.path.join(os.path.dirname(__file__), infile))
+        with open(infile, mode="r", encoding=encoding) as csvfile:
+            csvdata = csv.reader(csvfile=csvfile, **fmtparams)
+            # Todo: Stuff with csvdata
+    except Exception as exc:
+        raise CagingError from exc
+
+    raise NotImplementedError
+
+
+def uncage_csv(cage: DataCage, outfile: str) -> None:
+    """Release a csv file from it's cage to return to the wild in the file system
+
+    Args:
+        cage (DataCage): cage to open
+        outfile (str): relative path to relase the cage into
+
+    Raises:
+        NotImplementedError: _description_
+    """
     raise NotImplementedError
